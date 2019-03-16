0e31099685b13bd4d731e67cd61d3a7b53e05aaf
Buzut <blog@buzut.fr>
2019-03-15 19:26:44 -0500

    🌟 replace config.js by config.dist.js

---
+++ config.dist.js
@@ -0,0 +1,50 @@
+process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
+module.exports = {
+    siteUrl: 'https://my-blog.net', // without trailing slash
+    adminEmail: 'hello@my-blog.net',
+
+    db: {
+        // check options at https://knexjs.org/
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'blog_comments'
+        },
+
+        // no need to change these
+        usersTable: 'users',
+        articlesTable: 'articles',
+        commentsTable: 'comments',
+        notificationsTable: 'notifications'
+    },
+
+    email: {
+        // check options at https://www.npmjs.com/package/emailjs
+        server: {
+            user: 'hello@my-blog.net',
+            password: 'xxxxx',
+            host: 'smtp.your-email.com',
+            ssl: true
+            // tls: { ciphers: 'SSLv3' }
+        },
+        senderAddr: 'hello@my-blog.net',
+        commentValidationSubject: 'Confirm your comment on Buzut’s blog',
+
+        // %name% and %link% will be dynamically replaced by their respective values (\n goes to next line)
+        commentValidationBody: 'Hey %name%!\n\n Thank you very much for leaving a comment on my blog.\n\n Please, just to make sure you’re not a bot, click the following link:\n%link%\n\n Thank you :)'
+    },
+
+    // this doesn't need to be changed either
+    cacheDirs: {
+        ext: '.json',
+        article: './article/'
+    },
+
+    // production only,
+    // in development, logs are outputed to the console and ./applogs.log
+    appLogFile: './jamComments.log',
+    port: 8888
+};
