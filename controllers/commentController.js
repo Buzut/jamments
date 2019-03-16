a0d78da0e7fc68c61e6fe311dba7b4d8836a6b81
Buzut <blog@buzut.fr>
2019-03-16 10:28:20 -0500

    🚧 create custom request validator (DRY)

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,49 +1,14 @@
-const qs = require('querystring');
 const getFirstElementIfArray = require('../libs/getFirstElementIfArray');
 const { sendNewCommentValidationMail } = require('../libs/emailSenders');
+const isEmail = require('../libs/isEmail');
 const sendRes = require('../libs/sendRes');
-const { generateArticleCache } = require('../libs/cacheFilesGenerators');
+// const { generateArticleCache } = require('../libs/cacheFilesGenerators');
 const smartErrorHandler = require('../libs/smartErrorHandler');
-const { validateReqHeaders, validateReqLength, validateReqParams } = require('../libs/validators');
+const validateRequest = require('../libs/validateRequest');
 const articleModel = require('../models/articleModel');
 const commentModel = require('../models/commentModel');
 const notificationModel = require('../models/notificationModel');
 const userModel = require('../models/userModel');
-
-/**
- * Parse data from request object and validate its content
- * @param { Object } req
- * @return { Promise }
- */
-function parseReqData(req) {
-    let body = '';
-
-    return new Promise((resolve, reject) => {
-        req.on('data', (data) => {
-            body += data;
-
-            const reqTooBigErr = validateReqLength(body);
-
-            if (reqTooBigErr) reject(reqTooBigErr);
-        });
-
-        req.on('end', () => {
-            const post = qs.parse(body);
-            const missingParamErr = validateReqParams(post);
-
-            if (missingParamErr) {
-                reject(missingParamErr);
-                return;
-            }
-
-            resolve({
-                ...post,
-                notify: !(typeof post.notify !== 'undefined' && (!post.notify || post.notify === 'false')),
-                ip: req.connection.remoteAddress
-            });
-        });
-    });
-}
 
 /**
  * Save comment infos in their respective tables
@@ -78,30 +43,30 @@ function saveComment({ slug, parent_id, name, email, ip, comment, notify }) { //
 
 /**
  * Manage new comment
- * Parse the comment, save it to DB and re-generate cache files
+ * Parse the comment and save it to DB
  * @param { Object } req
  * @param { Object } res
  */
 function addComment(req, res) {
-    const handleErr = err => smartErrorHandler(err, res);
+    const paramsValidation = [
+        { slug: 'string' },
+        { name: 'name', type: 'string', validator: v => v.minLength(3).maxLength(20), failMsg: 'name must be between 3 and 20 chars' }, // eslint-disable-line
+        { name: 'email', customValidator: isEmail, failMsg: 'email must be an email string' },
+        { name: 'comment', type: 'string', validator: v => v.maxLength(3000), failMsg: 'comment cannot be longer than 3000 chars' }, // eslint-disable-line
+        { name: 'notify', type: 'boolean', optional: true }
+    ];
 
-    try {
-        validateReqHeaders(req.headers);
-
-        parseReqData(req)
-        .then(
-            postData => saveComment(postData)
-            .then(
-                commentValidationParams => sendRes(res, 201)
-                .then(() => sendNewCommentValidationMail(commentValidationParams))
-            )
-        )
-        .catch(handleErr);
-    }
-
-    catch (err) {
-        handleErr(err);
-    }
+    validateRequest(req, paramsValidation)
+    .then(post => saveComment({
+        ...post,
+        notify: !(typeof post.notify !== 'undefined' && (!post.notify || post.notify === 'false')),
+        ip: req.connection.remoteAddress
+    }))
+    .then(
+        commentValidationParams => Promise.resolve(sendRes(res, 201))
+        .then(() => sendNewCommentValidationMail(commentValidationParams))
+    )
+    .catch(err => smartErrorHandler(err, res));
 }
 
 module.exports = { addComment };
